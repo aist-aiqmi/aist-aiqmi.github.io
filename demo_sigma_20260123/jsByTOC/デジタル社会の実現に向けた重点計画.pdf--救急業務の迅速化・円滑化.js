b61643b1269405ca737b23d4452d3de8a6babad9
masahiko-sato-prime <sato@prime-intl.co.jp>
2026-04-08 19:03:17 +0900

    Add files via upload

---
+++ demo_sigma_20260123/jsByTOC/デジタル社会の実現に向けた重点計画.pdf--救急業務の迅速化・円滑化.js
@@ -0,0 +1,27 @@
+var toc_content = `このページで探していた情報は見つかりましたか？
+はい
+いいえ
+〒102-0094
+東京都千代田区紀尾井町1-3
+東京ガーデンテラス紀尾井町
+電話番号：(03)4477-6775（代表）
+法人番号：8000012010038
+サイトポリシー
+プライバシーポリシー
+SNSポリシー
+ウェブアクセシビリティ
+ご意見・ご要望
+サイトマップ
+RSS配信
+© Digital Agency, Government of Japan
+2024/12/20 10:50
+デジタル社会の実現に向けた重点計画｜デジタル庁
+https://www.digital.go.jp/policies/priority-policy-program
+29/30
+
+2024/12/20 10:50
+デジタル社会の実現に向けた重点計画｜デジタル庁
+https://www.digital.go.jp/policies/priority-policy-program
+30/30
+
+`;
