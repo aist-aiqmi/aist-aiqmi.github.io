c860bdd7f3ed41f6278f8f3a1447187d60cde6ea
masahiko-sato-prime <sato@prime-intl.co.jp>
2026-04-08 15:32:33 +0900

    Add files via upload

---
+++ demo_sigma_20260123/jsByTOC/AI Act.pdf--Article 32.js
@@ -0,0 +1,13 @@
+var toc_content = `Article 32
+Presumption of conformity with requirements relating to notified bodies
+Where a conformity assessment body demonstrates its conformity with the criteria laid down in the relevant harmonised 
+standards or parts thereof, the references of which have been published in the Official Journal of the European Union, it shall 
+be presumed to comply with the requirements set out in Article 31 in so far as the applicable harmonised standards cover 
+those requirements.
+EN
+OJ L, 12.7.2024
+72/144
+ELI: http://data.europa.eu/eli/reg/2024/1689/oj
+
+
+`;
