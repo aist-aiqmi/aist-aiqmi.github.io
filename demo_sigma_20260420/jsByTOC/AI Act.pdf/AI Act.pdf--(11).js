d1fa38518daab6429c2d97577f7424e658f2eb26
masahiko-sato-prime <sato@prime-intl.co.jp>
2026-04-21 14:10:18 +0900

    Add files via upload

---
+++ demo_sigma_20260420/jsByTOC/AI Act.pdf/AI Act.pdf--(11).js
@@ -0,0 +1,2 @@
+var toc_content = `(11) and (EU) 2018/1725 
+`;
---
+++ demo_sigma_20260420/jsByTOC/AI Act.pdf/AI Act.pdf--(26).js
@@ -0,0 +1,13 @@
+var toc_content = `(26)
+In order to introduce a proportionate and effective set of binding rules for AI systems, a clearly defined risk-based 
+approach should be followed. That approach should tailor the type and content of such rules to the intensity and 
+scope of the risks that AI systems can generate. It is therefore necessary to prohibit certain unacceptable AI practices, 
+to lay down requirements for high-risk AI systems and obligations for the relevant operators, and to lay down 
+transparency obligations for certain AI systems.
+OJ L, 12.7.2024
+EN
+ELI: http://data.europa.eu/eli/reg/2024/1689/oj
+7/144
+
+
+`;
