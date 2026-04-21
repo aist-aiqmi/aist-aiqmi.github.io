cf6e7bce0db3c9fc026fc1f51f24ebf4a8745294
masahiko-sato-prime <sato@prime-intl.co.jp>
2026-04-21 14:17:24 +0900

    Add files via upload

---
+++ demo_sigma_20260420/jsByTOC/AIセーフティに関する評価観点ガイド.pdf/AIセーフティに関する評価観点ガイド.pdf--はじめに.js
@@ -0,0 +1,3 @@
+var toc_content = `はじめに 
+1.1 
+`;
---
+++ demo_sigma_20260420/jsByTOC/AIセーフティに関する評価観点ガイド.pdf/AIセーフティに関する評価観点ガイド.pdf--参考文献一覧.js
@@ -0,0 +1,65 @@
+var toc_content = `参考文献一覧 
+➢ 
+外務省, 「高度なAI システムを開発する組織向けの広島プロセス国際指針」 
+https://www.mofa.go.jp/mofaj/files/100573469.pdf 
+➢ 
+外務省, 「高度なAI システムを開発する組織向けの広島プロセス国際行動規範」 
+https://www.mofa.go.jp/mofaj/files/100573472.pdf 
+➢ 
+総務省・経済産業省, 「AI 事業者ガイドライン（第1.0 版）」 
+https://www.soumu.go.jp/main_content/000943079.pdf 
+https://www.meti.go.jp/press/2024/04/20240419004/20240419004-1.pdf 
+➢ 
+産業技術総合研究所 「機械学習品質マネジメントガイドライン 第4 版」 
+https://www.digiarc.aist.go.jp/publication/aiqm/guideline-rev4.html 
+➢ 
+統合イノベーション戦略推進会議決定「人間中心のAI 社会原則」 
+https://www8.cao.go.jp/cstp/aigensoku.pdf 
+➢ 
+AI セーフティ・インスティテュート, 「AI セーフティに関するレッドチーミング手法ガイド」 
+https://aisi.go.jp/2024/09/25/rt_methodology/ 
+➢ 
+Department for Science, Innovation and Technology, UK AI Safety Institute 
+“International Scientific Report on the Safety of Advanced AI（Interim report）” 
+https://assets.publishing.service.gov.uk/media/6655982fdc15efdddf1a842f/intern
+ational_scientific_report_on_the_safety_of_advanced_ai_interim_report.pdf 
+➢ 
+Department for Science, Innovation and Technology, UK AI Safety Institute 
+“Introducing the AI Safety Institute” 
+https://www.gov.uk/government/publications/ai-safety-institute-
+overview/introducing-the-ai-safety-institute 
+➢ 
+Infocomm Media Development Authority, AI Verify Foundation “CATALOGUING 
+LLM EVALUATIONS Draft for Discussion （October 2023）” 
+https://aiverifyfoundation.sg/downloads/Cataloguing_LLM_Evaluations.pdf 
+➢ 
+Infocomm Media Development Authority, AI Verify Foundation “Model AI 
+Governance Framework for Generative AI” 
+https://aiverifyfoundation.sg/wp-content/uploads/2024/05/Model-AI-
+Governance-Framework-for-Generative-AI-May-2024-1-1.pdf 
+➢ 
+ISO/IEC JTC 1/SC 42 “ISO/IEC 42001:2023 情報技術－人工知能－マネジメントシステム 
+Information technology -- Artificial intelligence -- Management system” 
+https://www.iso.org/standard/81230.html 
+➢ 
+National Institute of Standards and Technology “Artificial Intelligence Risk 
+Management Framework （AI RMF 1.0）” 
+https://nvlpubs.nist.gov/nistpubs/ai/NIST.AI.100-1.pdf 
+
+ 
+30 
+ 
+➢ 
+NIST Trustworthy and Responsible AI NIST AI 600-1 Artificial Intelligence Risk 
+Management Framework: Generative Artificial Intelligence Profile. 
+https://nvlpubs.nist.gov/nistpubs/ai/NIST.AI.600-1.pdf 
+➢ 
+OWASP “OWASP Top 10 for Large Language Model Applications” 
+https://owasp.org/www-project-top-10-for-large-language-model-applications/ 
+➢ 
+Stanford Institute for Human-Centered Artificial Intelligence “Reflections on 
+Foundation Models” 
+https://hai.stanford.edu/news/reflections-foundation-models 
+ 
+
+`;
