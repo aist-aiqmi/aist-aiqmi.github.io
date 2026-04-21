cd24ee545b4faee346628014e22e4dd7bd25a1e4
masahiko-sato-prime <sato@prime-intl.co.jp>
2026-04-21 14:15:01 +0900

    Add files via upload

---
+++ demo_sigma_20260420/jsByTOC/AI Act.pdf/AI Act.pdf--Article 87.js
@@ -0,0 +1,8 @@
+var toc_content = `Article 87
+Reporting of infringements and protection of reporting persons
+Directive (EU) 2019/1937 shall apply to the reporting of infringements of this Regulation and the protection of persons 
+reporting such infringements.
+SECTION 5
+Supervision, investigation, enforcement and monitoring in respect of providers of general-purpose AI models
+
+`;
---
+++ demo_sigma_20260420/jsByTOC/AI Act.pdf/AI Act.pdf--Article 94.js
@@ -0,0 +1,8 @@
+var toc_content = `Article 94
+Procedural rights of economic operators of the general-purpose AI model
+Article 18 of Regulation (EU) 2019/1020 shall apply mutatis mutandis to the providers of the general-purpose AI model, 
+without prejudice to more specific procedural rights provided for in this Regulation.
+CHAPTER X
+CODES OF CONDUCT AND GUIDELINES
+
+`;
