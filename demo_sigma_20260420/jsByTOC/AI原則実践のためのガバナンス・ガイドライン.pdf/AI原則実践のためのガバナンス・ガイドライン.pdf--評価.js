f06b65c2db6b9aa86192671a479e390235790683
masahiko-sato-prime <sato@prime-intl.co.jp>
2026-04-21 15:14:25 +0900

    Add files via upload

---
+++ demo_sigma_20260420/jsByTOC/AI原則実践のためのガバナンス・ガイドライン.pdf/AI原則実践のためのガバナンス・ガイドライン.pdf--評価.js
@@ -0,0 +1,19 @@
+var toc_content = `評価や対応に関する情報に
+ついても、非財務情報の一部として開示されることが期待される場合があると考え
+られる。 
+この基本原則に関連し、本ガイドラインの作成の際に行ったヒアリングにおいて、
+欧州の機関投資家からAI ガバナンスについて問い合わせがあったという情報をヒ
+アリング先から得た。AI ガバナンスへの関心が投資家の間で少しずつ高まっている
+ことが、このような問い合わせの背景にあると考えられる。たとえば、Hermes EOS 
+(Equity Ownership Services)は、グーグルの親会社であるアルファベットの取締役
+会に対して、責任を持ったAI 技術の利用に関してリーダーシップを示すことを投
+資家が期待していると述べている25。AI システムの用途によっては、その利用の拡
+大に伴って社会へ与えるインパクトが大きくなりうるため、投資家から責任を持っ
+たAI システムの開発・運用やAI ガバナンスの整備が求められる可能性がある。 
+ 
+ 
+ 
+25 Alex Rolandi, “Hermes EOS urges Alphabet to lead responsible AI practice,” funds europe (June 18, 2019), 
+https://www.funds-europe.com/news/hermes-eos-urges-alphabet-to-lead-responsible-ai-practice.  
+
+`;
