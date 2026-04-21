e949fe4464e376209d3b73acac455aca5457d0a2
masahiko-sato-prime <sato@prime-intl.co.jp>
2026-04-21 14:11:16 +0900

    Add files via upload

---
+++ demo_sigma_20260420/jsByTOC/AI Act.pdf/AI Act.pdf--(38).js
@@ -0,0 +1,5 @@
+var toc_content = `(38) and Directive (EU) 2019/882. Providers should ensure 
+compliance with these requirements by design. Therefore, the necessary measures should be integrated as much as 
+possible into the design of the high-risk AI system.
+
+`;
