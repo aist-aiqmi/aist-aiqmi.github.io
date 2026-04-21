6feb0be73b4a9218e2ffe6984cd60a9987bd0bb0
masahiko-sato-prime <sato@prime-intl.co.jp>
2026-04-21 14:04:41 +0900

    Add files via upload

---
+++ demo_sigma_20260420/jsByTOC/AIセーフティに関するレッドチーミング手法ガイド.pdf/AIセーフティに関するレッドチーミング手法ガイド.pdf--A.2 参考文献一覧.js
@@ -0,0 +1,98 @@
+var toc_content = `56 
+ 
+A.2 
+参考文献一覧 
+➢ 
+機械学習工学研究会, 「機械学習システムセキュリティガイドライン version 2.00」  
+https://github.com/mlse-jssst/security-guideline 
+➢ 
+総務省・経済産業省, 「AI 事業者ガイドライン（第1.0 版）」 
+https://www.soumu.go.jp/main_content/000943079.pdf 
+https://www.meti.go.jp/press/2024/04/20240419004/20240419004-1.pdf 
+➢ 
+外務省, 「高度なAI システムを開発する組織向けの広島プロセス国際指針」 
+https://www.mofa.go.jp/mofaj/files/100573469.pdf 
+➢ 
+外務省, 「高度なAI システムを開発する組織向けの広島プロセス国際行動規範」 
+https://www.mofa.go.jp/mofaj/files/100573472.pdf 
+➢ 
+産業技術総合研究所 「機械学習品質マネジメントガイドライン 第4 版」 
+https://www.digiarc.aist.go.jp/publication/aiqm/guideline-rev4.html 
+➢ 
+Department for Science, Innovation and Technology, UK AI Safety Institute 
+“International Scientific Report on the Safety of Advanced AI（Interim 
+report）” 
+https://assets.publishing.service.gov.uk/media/6655982fdc15efdddf1a842f/in
+ternational_scientific_report_on_the_safety_of_advanced_ai_interim_report.p
+df 
+➢ 
+Department for Science, Innovation and Technology “Introducing the AI 
+Safety Institute” 
+https://www.gov.uk/government/publications/ai-safety-institute-
+overview/introducing-the-ai-safety-institute 
+➢ 
+Infocomm Media Development Authority, AI Verify Foundation 
+“CATALOGUING LLM EVALUATIONS Draft for Discussion (October 2023)” 
+https://aiverifyfoundation.sg/downloads/Cataloguing_LLM_Evaluations.pdf 
+➢ 
+Infocomm Media Development Authority, AI Verify Foundation “Model AI 
+Governance Framework for Generative AI” 
+https://aiverifyfoundation.sg/wp-content/uploads/2024/05/Model-AI-
+Governance-Framework-for-Generative-AI-May-2024-1-1.pdf 
+➢ 
+ISO/IEC JTC 1/SC 42 “ISO/IEC 42001:2023 情報技術－人工知能－マネジメントシス
+テム Information technology -- Artificial intelligence -- Management system” 
+https://www.iso.org/standard/81230.html 
+➢ 
+ISO/IEC 22989:2022 Information technology – Artificial Intelligence - 
+Artificial Intelligence concepts and terminology. 
+https://www.iso.org/standard/74296.html 
+➢ 
+National Institute of Standards and Technology “SP800-115 Technical Guide 
+
+57 
+ 
+to Information Security Testing and Assessment” 
+https://www.nist.gov/privacy-framework/nist-sp-800-115 
+➢ 
+National Institute of Standards and Technology “Artificial Intelligence Risk 
+Management Framework (AI RMF 1.0)” 
+https://nvlpubs.nist.gov/nistpubs/ai/NIST.AI.100-1.pdf 
+➢ 
+National Institute of Standards and Technology “Artificial Intelligence Risk 
+Management Framework: Generative Artificial Intelligence Profile” 
+https://nvlpubs.nist.gov/nistpubs/ai/NIST.AI.600-1.pdf 
+➢ 
+National Institute of Standards and Technology “AI 800-1 Managing Misuse 
+Risk for Dual-Use Foundation Models（Initial public draft）” 
+https://nvlpubs.nist.gov/nistpubs/ai/NIST.AI.800-1.ipd.pdf 
+➢ 
+OWASP “OWASP Top 10 for Large Language Model Applications” 
+https://owasp.org/www-project-top-10-for-large-language-model-
+applications/ 
+➢ 
+Stanford Institute for Human-Centered Artificial Intelligence “Reflections on 
+Foundation Models” 
+https://hai.stanford.edu/news/reflections-foundation-models 
+➢ 
+ANTHROPIC  ”Challenges in red teaming AI systems” 
+https://www.anthropic.com/news/challenges-in-red-teaming-ai-systems 
+➢ 
+Open AI “Open AI Red Teaming Network” 
+https://openai.com/index/red-teaming-network/ 
+➢ 
+MITRE ATLAS（Adversarial Thread Landscape for Artificial Intelligence 
+Systems） 
+https://atlas.mitre.org/ 
+➢ 
+OECD, AI Incidents Monitor（AIM） 
+https://oecd.ai/en/ 
+➢ 
+Partnership on AI, AI Incident Database 
+https://incidentdatabase.ai/ 
+➢ 
+AI セーフティ・インスティテュート 「AI セーフティに関する評価観点ガイド」 
+https://aisi.go.jp/2024/09/18/evaluation_perspectives/ 
+ 
+
+`;
